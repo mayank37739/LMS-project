a82819d9674711df34b14fad3ff6c2cac9a6a7cf
Gauransh <mayankkumar37739@gmail.com>
2026-04-19 20:53:51 +0530

    serverjs commented

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 // import connectCloudinary from './configs/cloudinary.js'
-import userRouter from './routes/userRoutes.js'
+// import userRouter from './routes/userRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
 import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
 // import educatorRouter from './routes/educatorRoutes.js'
@@ -26,7 +26,7 @@ app.post('/clerk', express.json() , clerkWebhooks)
 // app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 // app.use('/api/educator', express.json(), educatorRouter)
 // app.use('/api/course', express.json(), courseRouter)
-app.use('/api/user', express.json(), userRouter)
+// app.use('/api/user', express.json(), userRouter)
 
 // Port
 const PORT = process.env.PORT || 5000
